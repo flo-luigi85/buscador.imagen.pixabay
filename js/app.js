0f2bf08b82e9cc18e3f1bbdb85ef8a6c66db1a73
flo-luigi85 <flo.lugi85@gmail.com>
2022-11-13 23:30:43 -0300

    agrego async

--- js/app.js
+++ js/app.js
@@ -55,18 +55,27 @@ function mostrarAlerta(mensaje) {
   }
 }
 
-function buscarImagenes() {
+async function buscarImagenes() {
 
   const termino = document.querySelector("#termino").value;
 
   const key = "31301138-29d7e292be5a2ab39315d3e9f";
   const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${registrosPorPagina}&page=${paginaActual}`;
-  fetch(url)
+  /* fetch(url)
     .then((respuesta) => respuesta.json())
     .then((resultado) => {
       totalPaginas = calcularPaginas(resultado.totalHits);
       mostrarImagenes(resultado.hits);
-    });
+    }); */
+    try{
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+      totalPaginas = calcularPaginas(resultado.totalHits);
+      mostrarImagenes(resultado.hits);
+    }catch (error){
+      console.log(error);
+    }
+    
 }
 // Generador que va a registrar la cantidad de elementos de acuerdo a las paginas
 function* crearPaginador(total) {
